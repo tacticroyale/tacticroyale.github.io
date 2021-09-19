86449acda5c0f8e953735dd10a523b9c0c4da57a
tacticroyale <91003853+tacticroyale@users.noreply.github.com>
2021-09-19 00:38:07 -0700

    Create gameSettings.js

---
+++ gameSettings.js
@@ -0,0 +1,12 @@
+function gameSettings() {
+  this.playerCount = 6;
+  this.mapSize = 15;
+  this.pointsPerRound = 1;
+  this.roundTime = 1; //in hours
+  this.juryVotes = 3;
+  this.startingRange = 1;
+  this.startingHealth = 3;
+  this.regainHealth = true
+  this.revives = true;
+}
+
